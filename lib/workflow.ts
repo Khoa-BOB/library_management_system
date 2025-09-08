ad60e56216b7e012ccee2564c8ef2f035eac7eed
NGUYEN ANH KHOA TRAN <trannguyenanhkhoa05122003@gmail.com>
2025-09-07 21:41:42 -0500

    Set up Resend and add upstach resend email workflow

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,33 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "@/lib/config";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qtashClient = new QStashClient({ token: config.env.upstash.qstashToken });
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qtashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "KhoaTran <library.khoa-hcmc.com>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
